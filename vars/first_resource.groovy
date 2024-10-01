5d59f87f14aba5c1dc36f226114a7e80cf85f117
Deepti <93430410+deepti-shitole@users.noreply.github.com>
2024-10-01 22:46:13 +0530

    Update first_resource.groovy

--- vars/first_resource.groovy
+++ vars/first_resource.groovy
@@ -1,3 +1,5 @@
 def call(){
   bat "date /T"
 }
+//. On Windows, the date command expects user input for setting the date, but in a Jenkins pipeline, interactive prompts won't work since the pipeline runs in a non-interactive shell.
+//To fix this, you can use date /T in the bat command to print the current date without prompting for input:
